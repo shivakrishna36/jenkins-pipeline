e5dcf5f7c716cda1e87e6aacaa0d843b02288836
SHIVA KRISHNA <45350264+shivakrishna36@users.noreply.github.com>
2020-03-31 20:48:56 +0530

    second commit

--- src/main/java/com/example/pipeline/PipelineApplication.java
+++ src/main/java/com/example/pipeline/PipelineApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PipelineApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
+		String option = argv[0];
 		SpringApplication.run(PipelineApplication.class, args);
 	}
 
